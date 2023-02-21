8104893a0c3e39de5d014a4d0534e01e1f98fe1f
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-02-20 21:36:59 -0300

    ⚒

--- src/infra/http/dtos/create-notification-body.ts
+++ src/infra/http/dtos/create-notification-body.ts
@@ -1,11 +1,11 @@
 import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationsBody {
-  @IsNotEmpty() // Validação. 0 recipente não pode estar vazio
+  @IsNotEmpty()
   @IsUUID()
   recipientId!: string;
 
-  @Length(5, 255) // Validação 2 content deve ter entre 5 e 255 caracteres
+  @Length(5, 255)
   @IsNotEmpty()
   content!: string;
 
